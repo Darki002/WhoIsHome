f55d42a7b9d3384002da352556dbd84fa4fc22c4
Darki002 <darkizockt16@gmail.com>
2024-06-27 16:50:59 +0200

    Update NewEventMessageService.kt

--- app/app/src/main/java/ch/darki/whoishome/core/notifications/NewEventMessageService.kt
+++ app/app/src/main/java/ch/darki/whoishome/core/notifications/NewEventMessageService.kt
@@ -1,8 +1,14 @@
 package ch.darki.whoishome.core.notifications
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 import ch.darki.whoishome.R
+import ch.darki.whoishome.mainActivity.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -22,17 +28,19 @@ class NewEventMessageService : FirebaseMessagingService() {
         // notify(remoteMessage)
     }
 
-    private fun notify(remoteMessage: RemoteMessage) {
-        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+    private fun notify(personName : String) {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Neues Event!")
-            .setContentText("${remoteMessage.data["person-name"]} hat gerade ein neues Event für Heute eingetragen")
+            .setContentText("$personName hat gerade ein neues Event für Heute eingetragen")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
 
-        // TODO show notification
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
     }
 
     companion object {
         const val CHANNEL_ID = "NewEvent"
+        const val NOTIFICATION_ID = 0
     }
 }
