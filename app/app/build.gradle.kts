88a0e8ccd8bd87c04eaa20a3bf750ed2f4f9b431
Darki002 <darkizockt16@gmail.com>
2024-06-27 15:44:02 +0200

    notification builder

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.core:core:2.2.0")
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
--- app/app/src/main/java/ch/darki/whoishome/core/notifications/NewEventMessageService.kt
+++ app/app/src/main/java/ch/darki/whoishome/core/notifications/NewEventMessageService.kt
@@ -1,6 +1,8 @@
 package ch.darki.whoishome.core.notifications
 
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import ch.darki.whoishome.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -14,10 +16,23 @@ class NewEventMessageService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         Log.d("NewEventMessageService", "Message data: ${remoteMessage.data}")
-        // Display push notification
+
+        // TODO filter if Notification is relevant
+
+        // notify(remoteMessage)
     }
 
     private fun notify(remoteMessage: RemoteMessage) {
-        
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Neues Event!")
+            .setContentText("${remoteMessage.data["person-name"]} hat gerade ein neues Event für Heute eingetragen")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        // TODO show notification
+    }
+
+    companion object {
+        const val CHANNEL_ID = "NewEvent"
     }
 }
