bfd61d12a48b26d9156e43bc2101ea4b34c839ea
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-07-19 09:41:33 +0200

    Update ServiceBase.cs

--- WhoIsHome/ServiceBase.cs
+++ WhoIsHome/ServiceBase.cs
@@ -3,30 +3,30 @@ using Google.Cloud.Firestore;
 
 namespace WhoIsHome;
 
-public abstract class ServiceBase<T>(FirestoreDb firestoreDb) where T : class
+public abstract class ServiceBase<TDbModel>(FirestoreDb firestoreDb) where TDbModel : class
 {
     protected readonly FirestoreDb FirestoreDb = firestoreDb;
     
     protected abstract string Collection { get; }
     
-    public async Task<Result<T, string>> GetAsync(string id)
+    public async Task<Result<TDbModel, string>> GetAsync(string id)
     {
         var result = await FirestoreDb.Collection(Collection)
             .Document(id)
             .GetSnapshotAsync();
 
         return result is null 
-            ? $"Can't find {typeof(T).Name} with Id {id}" 
+            ? $"Can't find {typeof(TDbModel).Name} with Id {id}" 
             : ConvertDocument(result);
     }
 
-    protected Result<T, string> ConvertDocument(DocumentSnapshot documentSnapshot)
+    protected Result<TDbModel, string> ConvertDocument(DocumentSnapshot documentSnapshot)
     {
-        var dbModel = documentSnapshot.ConvertTo<T>();
+        var dbModel = documentSnapshot.ConvertTo<TDbModel>();
 
         if (dbModel is null)
         {
-            return $"Can't convert {documentSnapshot} to type ${typeof(T).Name}";
+            return $"Can't convert {documentSnapshot} to type ${typeof(TDbModel).Name}";
         }
 
         return dbModel;
