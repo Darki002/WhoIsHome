26adcb865868ea2e8fb02239272f77c74681cb60
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-07-19 08:47:29 +0200

    Convert Document to Model

--- WhoIsHome/Persons/Person.cs
+++ WhoIsHome/Persons/Person.cs
@@ -1,8 +1,18 @@
+using Google.Cloud.Firestore;
+
 namespace WhoIsHome.Persons;
 
+[FirestoreData]
 public class Person
 {
+    [FirestoreDocumentId]
     public string? Id { get; set; }
+    
+    
+    [FirestoreProperty("displayName")]
     public string DisplayName { get; set;  } = null!;
+    
+    
+    [FirestoreProperty("email")]
     public string Email { get; set; } = null!;
 }
