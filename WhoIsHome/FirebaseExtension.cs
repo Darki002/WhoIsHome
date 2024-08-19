90f1aeedaf16a4ed2e464a5edaa61a3a40066b33
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-08-19 21:21:20 +0200

    unused

--- WhoIsHome/FirebaseExtension.cs
+++ WhoIsHome/FirebaseExtension.cs
@@ -5,11 +5,6 @@ namespace WhoIsHome;
 
 public static class FirebaseExtension
 {
-    public static Query WherePersonIs(this Query query, Person person)
-    {
-        return query.WhereEqualTo("person:id", person.Id);
-    }
-    
     public static Query WherePersonIs(this Query query, string personId)
     {
         return query.WhereEqualTo("person:id", personId);
