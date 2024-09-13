777e65e1fb34d9c5089c59d55029b788d0a68085
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-09-13 20:31:26 +0200

    clean up setup

---
+++ WhoIsHome.Host/SetUp/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace WhoIsHome.Host.SetUp;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.EnableAnnotations();
+        });
+        services.AddControllers();
+        
+        services.AddWhoIsHomeServices(configuration);
+        return services;
+    }
+}
