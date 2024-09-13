1af23cca56a8598769f6c326f7fd9a0fb2916227
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-09-13 20:16:53 +0200

    add swagger to production

--- WhoIsHome.Host/Program.cs
+++ WhoIsHome.Host/Program.cs
@@ -18,9 +18,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseRouting();
 app.MapControllers();
