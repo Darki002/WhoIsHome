89dde78eedcfda44b8202d2b15763abf56eb67ed
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-09-24 21:40:05 +0200

    Update ApiKeyMiddleware.cs

--- WhoIsHome.Host/Authentication/ApiKeyMiddleware.cs
+++ WhoIsHome.Host/Authentication/ApiKeyMiddleware.cs
@@ -1,30 +1,23 @@
 ﻿namespace WhoIsHome.Host.Authentication;
 
-public class ApiKeyMiddleware
+public class ApiKeyMiddleware(RequestDelegate next)
 {
     public const string ApiKeyHeaderName = "X-API-KEY";
-    private readonly string apiKey;
-    private readonly RequestDelegate next;
-
-    public ApiKeyMiddleware(RequestDelegate next)
-    {
-        this.next = next;
-        apiKey = Environment.GetEnvironmentVariable("API_KEY") ??
-                 throw new Exception("API_KEY not found in environment variables.");
-    }
+    private readonly string apiKey = Environment.GetEnvironmentVariable("API_KEY") ??
+                                     throw new Exception("API_KEY not found in environment variables.");
 
     public async Task InvokeAsync(HttpContext context, IConfiguration configuration)
     {
         if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
         {
-            context.Response.StatusCode = 401; // Unauthorized
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             await context.Response.WriteAsync("API Key is missing.");
             return;
         }
 
         if (!apiKey.Equals(extractedApiKey))
         {
-            context.Response.StatusCode = 401; // Unauthorized
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             await context.Response.WriteAsync("Unauthorized access.");
             return;
         }
