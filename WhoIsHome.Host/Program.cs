6d420de40c7c03beb5ace49a0d2c2299c1f23894
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-09-25 10:02:11 +0200

    configer logger

--- WhoIsHome.Host/Program.cs
+++ WhoIsHome.Host/Program.cs
@@ -8,6 +8,9 @@ DotNetEnv.Env.Load();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddJwtAuthentication(builder.Configuration);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 var app = builder.Build();
 app.ConfigureApplication();
 app.Run();
