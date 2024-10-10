bf059ac12e5b6d01f5b3dc4e438b435b9d320cc9
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-10 09:32:20 +0200

    Create DinnerTimeTest.cs

---
+++ WhoIsHome.Test/Application/Aggregates/DinnerTimeTest.cs
@@ -0,0 +1,235 @@
+using WhoIsHome.Aggregates;
+using WhoIsHome.Shared.Exceptions;
+using WhoIsHome.Shared.Types;
+
+namespace WhoIsHome.Test.Application.Aggregates;
+
+[TestFixture]
+public class DinnerTimeTest
+{
+    private readonly TimeOnly timeOnly = new TimeOnly(13, 30, 0);
+
+    private class Create : DinnerTimeTest
+    {
+        [Test]
+        public void ReturnsNewDinnerTime_FromUnknownType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Unknown;
+
+            // Act
+            var result = DinnerTime.Create(presenceType, null);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().BeNull();
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_UnknownTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Unknown;
+
+            // Act
+            var result = () => DinnerTime.Create(presenceType, timeOnly);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+
+        [Test]
+        public void ReturnsNewDinnerTime_FromDefaultType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Default;
+
+            // Act
+            var result = DinnerTime.Create(presenceType, timeOnly);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().Be(timeOnly);
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_DefaultTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Default;
+
+            // Act
+            var result = () => DinnerTime.Create(presenceType, null);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+
+        [Test]
+        public void ReturnsNewDinnerTime_FromLateType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Late;
+
+            // Act
+            var result = DinnerTime.Create(presenceType, timeOnly);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().Be(timeOnly);
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_LateTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Late;
+
+            // Act
+            var result = () => DinnerTime.Create(presenceType, null);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+
+        [Test]
+        public void ReturnsNewDinnerTime_FromNotPresentType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.NotPresent;
+
+            // Act
+            var result = DinnerTime.Create(presenceType, null);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().BeNull();
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_NotPresentTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.NotPresent;
+
+            // Act
+            var result = () => DinnerTime.Create(presenceType, timeOnly);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+    }
+    
+    private class Update : DinnerTimeTest
+    {
+        private readonly DinnerTime dinnerTime = new DinnerTime(PresenceType.Unknown, null);
+        
+        [Test]
+        public void ReturnsNewDinnerTime_FromUnknownType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Unknown;
+
+            // Act
+            var result = dinnerTime.Update(presenceType, null);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().BeNull();
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_UnknownTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Unknown;
+
+            // Act
+            var result = () => dinnerTime.Update(presenceType, timeOnly);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+
+        [Test]
+        public void ReturnsNewDinnerTime_FromDefaultType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Default;
+
+            // Act
+            var result = dinnerTime.Update(presenceType, timeOnly);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().Be(timeOnly);
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_DefaultTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Default;
+
+            // Act
+            var result = () => dinnerTime.Update(presenceType, null);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+
+        [Test]
+        public void ReturnsNewDinnerTime_FromLateType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Late;
+
+            // Act
+            var result = dinnerTime.Update(presenceType, timeOnly);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().Be(timeOnly);
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_LateTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.Late;
+
+            // Act
+            var result = () => dinnerTime.Update(presenceType, null);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+
+        [Test]
+        public void ReturnsNewDinnerTime_FromNotPresentType()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.NotPresent;
+
+            // Act
+            var result = dinnerTime.Update(presenceType, null);
+
+            // Assert
+            result.PresenceType.Should().Be(presenceType);
+            result.Time.Should().BeNull();
+        }
+
+        [Test]
+        public void ThrowsInvalidModelException_NotPresentTimeWithTime()
+        {
+            // Arrange
+            const PresenceType presenceType = PresenceType.NotPresent;
+
+            // Act
+            var result = () => dinnerTime.Update(presenceType, timeOnly);
+
+            // Assert
+            result.Should().Throw<InvalidModelException>();
+        }
+    }
+}
--- WhoIsHome.Test/Application/Aggregates/UserTest.cs
+++ WhoIsHome.Test/Application/Aggregates/UserTest.cs
@@ -6,51 +6,54 @@ namespace WhoIsHome.Test.Application.Aggregates;
 [TestFixture]
 public class UserTest
 {
-    [Test]
-    public void Create_ReturnsNewUser_RepresentingAUserFromTheGivenData()
+    private class Create
     {
-        // Arrange
-        const string userName = "Darki";
-        const string email = "darki@whoishome.dev";
-        const string password = "securePassword1234";
+        [Test]
+        public void ReturnsNewUser_RepresentingAUserFromTheGivenData()
+        {
+            // Arrange
+            const string userName = "Darki";
+            const string email = "darki@whoishome.dev";
+            const string password = "securePassword1234";
         
-        // Act
-        var result = User.Create(userName, email, password);
+            // Act
+            var result = User.Create(userName, email, password);
         
-        // Assert
-        result.Id.Should().BeNull();
-        result.UserName.Should().Be(userName);
-        result.Email.Should().Be(email);
-        result.Password.Should().Be(password);
-    }
+            // Assert
+            result.Id.Should().BeNull();
+            result.UserName.Should().Be(userName);
+            result.Email.Should().Be(email);
+            result.Password.Should().Be(password);
+        }
 
-    [Test]
-    public void Create_ThrowsFormatException_WhenEmailIsInvalidFormat()
-    {
-        // Arrange
-        const string userName = "Darki";
-        const string invalidEmail = "missingAt.dev";
-        const string password = "securePassword1234";
+        [Test]
+        public void ThrowsFormatException_WhenEmailIsInvalidFormat()
+        {
+            // Arrange
+            const string userName = "Darki";
+            const string invalidEmail = "missingAt.dev";
+            const string password = "securePassword1234";
         
-        // Act
-        var act = () => User.Create(userName, invalidEmail, password);
+            // Act
+            var act = () => User.Create(userName, invalidEmail, password);
         
-        // Assert
-        act.Should().Throw<FormatException>();
-    }
+            // Assert
+            act.Should().Throw<FormatException>();
+        }
     
-    [Test]
-    public void Create_ThrowsInvalidModelException_WhenUserNameIsTooLong()
-    {
-        // Arrange
-        var userName = string.Join("", Enumerable.Repeat('a', 32));
-        const string invalidEmail = "darki@whoishome.dev";
-        const string password = "securePassword1234";
+        [Test]
+        public void ThrowsInvalidModelException_WhenUserNameIsTooLong()
+        {
+            // Arrange
+            var userName = string.Join("", Enumerable.Repeat('a', 32));
+            const string invalidEmail = "darki@whoishome.dev";
+            const string password = "securePassword1234";
         
-        // Act
-        var act = () => User.Create(userName, invalidEmail, password);
+            // Act
+            var act = () => User.Create(userName, invalidEmail, password);
         
-        // Assert
-        act.Should().Throw<InvalidModelException>();
+            // Assert
+            act.Should().Throw<InvalidModelException>();
+        }
     }
 }
