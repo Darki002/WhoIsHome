e9f482af3bff549924f16ac630ceb34f866c6adf
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-10 09:56:38 +0200

    Update OneTimeEventTest.cs

--- WhoIsHome.Test/Application/Aggregates/OneTimeEventTest.cs
+++ WhoIsHome.Test/Application/Aggregates/OneTimeEventTest.cs
@@ -1,10 +1,151 @@
+using WhoIsHome.Aggregates;
+using WhoIsHome.Shared.Exceptions;
+using WhoIsHome.Shared.Types;
+// ReSharper disable LocalVariableHidesMember
+
 namespace WhoIsHome.Test.Application.Aggregates;
 
 [TestFixture]
 public class OneTimeEventTest
 {
+    private const string Title = "Title";
+    private const int UserId = 0;
+    private const PresenceType PresenceType = WhoIsHome.Shared.Types.PresenceType.Unknown;
+    private readonly DateOnly date = new DateOnly(2024, 10, 10);
+    private readonly TimeOnly startTime = new TimeOnly(13, 30, 0);
+    private readonly TimeOnly endTime = new TimeOnly(14, 0, 0);
+    private readonly TimeOnly? time = new TimeOnly(18, 0, 0);
+    
     private class Create : OneTimeEventTest
     {
+        [Test]
+        public void ReturnsNewOneTimeEvent()
+        {
+            // Arrange
+            
+            // Act
+            var result = OneTimeEvent.Create(Title, date, startTime, endTime, PresenceType, null, UserId);
+            
+            // Assert
+            result.Id.Should().BeNull();
+            result.Title.Should().Be(Title);
+            result.Date.Should().Be(date);
+            result.StartTime.Should().Be(startTime);
+            result.EndTime.Should().Be(endTime);
+            result.DinnerTime.PresenceType.Should().Be(PresenceType);
+            result.DinnerTime.Time.Should().BeNull();
+            result.UserId.Should().Be(UserId);
+        }
         
+        [Test]
+        public void ThrowsInvalidModelException_WhenTitleIsTooLong()
+        {
+            // Arrange
+            var title = string.Join("", Enumerable.Repeat("a", 51));
+            
+            // Act
+            var act = () => OneTimeEvent.Create(title, date, startTime, endTime, PresenceType, null, UserId);
+            
+            // Assert
+            act.Should().Throw<InvalidModelException>();
+        }
+        
+        [Test]
+        public void ThrowsInvalidModelException_WhenStartTimeIsBeforeEndTime()
+        {
+            // Arrange
+            var startTime = new TimeOnly(13, 0, 0);
+            var endTime = new TimeOnly(12, 0, 0);
+            
+            // Act
+            var act = () => OneTimeEvent.Create(Title, date, startTime, endTime, PresenceType, null, UserId);
+            
+            // Assert
+            act.Should().Throw<InvalidModelException>();
+        }
+        
+        [Test]
+        public void ThrowsInvalidModelException_WhenEndTimeIsBeforeDinnerTime()
+        {
+            // Arrange
+            var time = endTime.AddHours(-1);
+            
+            // Act
+            var act = () => OneTimeEvent.Create(Title, date, startTime, endTime, PresenceType.Late, time, UserId);
+            
+            // Assert
+            act.Should().Throw<InvalidModelException>();
+        }
+    }
+    
+    private class Update : OneTimeEventTest
+    {
+        private OneTimeEvent oneTimeEvent = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var dinnerTime = new DinnerTime(PresenceType, time);
+            oneTimeEvent = new OneTimeEvent(null, Title, date, startTime, endTime, dinnerTime, UserId);
+        }
+        
+        [Test]
+        public void ReturnsNewOneTimeEvent()
+        {
+            // Arrange
+            
+            // Act
+            oneTimeEvent.Update(Title, date, startTime, endTime, PresenceType, null);
+            
+            // Assert
+            oneTimeEvent.Id.Should().BeNull();
+            oneTimeEvent.Title.Should().Be(Title);
+            oneTimeEvent.Date.Should().Be(date);
+            oneTimeEvent.StartTime.Should().Be(startTime);
+            oneTimeEvent.EndTime.Should().Be(endTime);
+            oneTimeEvent.DinnerTime.PresenceType.Should().Be(PresenceType);
+            oneTimeEvent.DinnerTime.Time.Should().BeNull();
+            oneTimeEvent.UserId.Should().Be(UserId);
+        }
+        
+        [Test]
+        public void ThrowsInvalidModelException_WhenTitleIsTooLong()
+        {
+            // Arrange
+            var title = string.Join("", Enumerable.Repeat("a", 51));
+            
+            // Act
+            var act = () => oneTimeEvent.Update(title, date, startTime, endTime, PresenceType, null);
+            
+            // Assert
+            act.Should().Throw<InvalidModelException>();
+        }
+        
+        [Test]
+        public void ThrowsInvalidModelException_WhenStartTimeIsBeforeEndTime()
+        {
+            // Arrange
+            var startTime = new TimeOnly(13, 0, 0);
+            var endTime = new TimeOnly(12, 0, 0);
+            
+            // Act
+            var act = () => oneTimeEvent.Update(Title, date, startTime, endTime, PresenceType, null);
+            
+            // Assert
+            act.Should().Throw<InvalidModelException>();
+        }
+        
+        [Test]
+        public void ThrowsInvalidModelException_WhenEndTimeIsBeforeDinnerTime()
+        {
+            // Arrange
+            var time = endTime.AddHours(-1);
+            
+            // Act
+            var act = () => oneTimeEvent.Update(Title, date, startTime, endTime, PresenceType.Late, time);
+            
+            // Assert
+            act.Should().Throw<InvalidModelException>();
+        }
     }
 }
