a04eea440069d6df6cd4f466389159b7534d0e93
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-10 13:30:10 +0200

    DaysUntilNext

--- WhoIsHome.Shared/Helper/DateOnlyHelper.cs
+++ WhoIsHome.Shared/Helper/DateOnlyHelper.cs
@@ -4,12 +4,12 @@ public static class DateOnlyHelper
 {
     public static DateOnly Today => DateOnly.FromDateTime(DateTime.Today);
     
-    public static bool IsThisWeek(this DateOnly dateOnly1)
+    public static bool IsThisWeek(this DateOnly dateOnly)
     {
-        return IsThisWeek(dateOnly1, DateTime.Today);
+        return dateOnly.IsSameWeek(DateTime.Today);
     }
 
-    internal static bool IsThisWeek(DateOnly dateOnly, DateTime dateTime)
+    public static bool IsSameWeek(this DateOnly dateOnly, DateTime dateTime)
     {
         var date1 = dateOnly.ToDateTime(TimeOnly.MinValue);
         var date2 = dateTime.Date;
@@ -20,9 +20,26 @@ public static class DateOnlyHelper
         return d1 == d2;
     }
     
+    public static bool IsSameWeek(this DateOnly dateOnly, DateOnly dateOnly2)
+    {
+        var dateTime = dateOnly2.ToDateTime(TimeOnly.MinValue);
+        return dateOnly.IsSameWeek(dateTime);
+    }
+    
     public static DateTime StartOfWeek(this DateTime dt)
     {
         var diff = (7 + (dt.DayOfWeek - DayOfWeek.Monday)) % 7;
         return dt.AddDays(-1 * diff).Date;
+    }
+
+    public static int DaysUntilNext(this DateTime date, DayOfWeek targetDay)
+    {
+        var difference = targetDay.Normalize() - date.DayOfWeek.Normalize();
+        return difference < 0 ? difference + 7 : difference;
+    }
+
+    private static DayOfWeek Normalize(this DayOfWeek dayOfWeek)
+    {
+        return (DayOfWeek)(((int)dayOfWeek + 6) % 7);
     }
 }
