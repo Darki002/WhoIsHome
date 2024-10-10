183b817df426eca42f7f71a1e492bea318dcd5c6
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-10 15:00:55 +0200

    Create UserContextFake.cs

---
+++ WhoIsHome.Test/UserContextFake.cs
@@ -0,0 +1,37 @@
+using WhoIsHome.Aggregates;
+using WhoIsHome.Shared.Authentication;
+
+namespace WhoIsHome.Test.Application.Services;
+
+public class UserContextFake : IUserContext
+{
+    private AuthenticatedUser authenticatedUser = null!;
+    
+    public int UserId { get; } = 0;
+    
+    public Task<AuthenticatedUser> GetCurrentUserAsync()
+    {
+        return Task.FromResult(authenticatedUser);
+    }
+
+    public Task<AuthenticatedUser> GetCurrentUserAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(authenticatedUser);
+    }
+
+    public bool IsUserPermitted(int permittedUserId)
+    {
+        return authenticatedUser.Id == permittedUserId;
+    }
+
+    public void SetUser(User user)
+    {
+        authenticatedUser = new AuthenticatedUser
+        {
+            Id = user.Id!.Value,
+            UserName = user.UserName,
+            Email = user.Email,
+            PasswordHash = user.Password
+        };
+    }
+}
