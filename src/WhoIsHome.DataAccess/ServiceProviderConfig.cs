3c3ffb27220f08a42746dbc062a87c0b6726751f
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-16 20:25:27 +0200

    connection string use env var in

--- src/WhoIsHome.DataAccess/ServiceProviderConfig.cs
+++ src/WhoIsHome.DataAccess/ServiceProviderConfig.cs
@@ -8,10 +8,22 @@ namespace WhoIsHome.DataAccess;
 
 public static class ServiceProviderConfig
 {
-    public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddDataAccessServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("MySql");
+        var connectionString = BuildConnectionString();
         services.AddDbContext<WhoIsHomeContext>(o => o.UseMySQL(connectionString!));
         return services;
-    } 
+    }
+
+    private static string BuildConnectionString()
+    {
+        var server = EnvironmentHelper.GetVariable(EnvVariables.MySqlServer);
+        var port = EnvironmentHelper.GetVariable(EnvVariables.MySqlPort);
+        var database = EnvironmentHelper.GetVariable(EnvVariables.MySqlDatabase);
+        var user = EnvironmentHelper.GetVariable(EnvVariables.MySqlUser);
+        var password = EnvironmentHelper.GetVariable(EnvVariables.MySqlPassword);
+
+        return $"Server={server};Port={port};Database={database};User={user};Password={password};";
+    }
 }
