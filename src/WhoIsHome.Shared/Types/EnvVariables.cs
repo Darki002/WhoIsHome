ffed8c0f4ba0cdbf4e59b7a8e6e6420b15236346
Darki002 <113591988+Darki002@users.noreply.github.com>
2024-10-16 20:50:19 +0200

    Better env variables (#14)
    * connection string use env var in
    
    * remove connection string from json

--- src/WhoIsHome.Shared/Types/EnvVariables.cs
+++ src/WhoIsHome.Shared/Types/EnvVariables.cs
@@ -11,4 +11,29 @@ public static class EnvVariables
     /// API Key used by the middleware in every request to Authorized.
     /// </summary>
     public const string ApiKey = "API_KEY";
+
+    /// <summary>
+    /// The SQL Server.
+    /// </summary>
+    public const string MySqlServer = "MYSQL_SERVER";
+    
+    /// <summary>
+    /// Port for the Database.
+    /// </summary>
+    public const string MySqlPort = "MYSQL_PORT";
+    
+    /// <summary>
+    /// Database that will be used by the Application.
+    /// </summary>
+    public const string MySqlDatabase = "MYSQL_DATABASE";
+    
+    /// <summary>
+    /// The User that is being used by the app to connect to the db.
+    /// </summary>
+    public const string MySqlUser = "MYSQL_USER";
+    
+    /// <summary>
+    /// The Password for the user that is being used by the app to connect to the db.
+    /// </summary>
+    public const string MySqlPassword = "MYSQL_PASSWORD";
 }
