cfccfbfe8dc5fb1211f719278315a469569977ef
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-20 20:03:48 +0200

    Create Mapper.cs

---
+++ src/WhoIsHome/AuthTokens/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using WhoIsHome.DataAccess.Models;
+
+namespace WhoIsHome.AuthTokens;
+
+public static class Mapper
+{
+    public static RefreshTokenModel ToModel(this RefreshToken refreshToken)
+    {
+        var model = new RefreshTokenModel
+        {
+            Token = refreshToken.Token,
+            Issued = refreshToken.Issued
+        };
+            
+        if (refreshToken.Id.HasValue)
+        {
+            model.Id = refreshToken.Id.Value;
+        }
+        return model;
+    }
+
+    public static RefreshToken ToRefreshToken(this RefreshTokenModel model)
+    {
+        return new RefreshToken(model.Id, model.Token, model.Issued);
+    }
+}
