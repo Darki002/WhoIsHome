a2751a67de6e0688fab506360cfb1d9c1423ea7f
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-20 20:03:46 +0200

    Create RefreshToken.cs

---
+++ src/WhoIsHome/AuthTokens/RefreshToken.cs
@@ -0,0 +1,19 @@
+﻿namespace WhoIsHome.AuthTokens;
+
+public class RefreshToken(int? id, string token, DateTime? issued)
+{
+    private const int ExpiresInDays = 12;
+
+    public int? Id { get; private init; } = id;
+    
+    public string Token { get; private init; } = token;
+
+    public DateTime Issued { get; private set; } = issued ?? DateTime.Now;
+
+    public bool IsValid => GetExpireDate() <= DateTime.Now;
+
+    public DateTime GetExpireDate()
+    {
+        return Issued.AddDays(ExpiresInDays);
+    }
+}
