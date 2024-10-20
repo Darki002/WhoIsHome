cd8943d73e3f26fd6756da05ada83b15f62c1ede
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-20 20:03:37 +0200

    Create RefreshTokenModel.cs

---
+++ src/WhoIsHome.DataAccess/Models/RefreshTokenModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using WhoIsHome.Shared.BaseTypes;
+
+namespace WhoIsHome.DataAccess.Models;
+
+[Table("RefreshToken")]
+[Index(nameof(Token), IsUnique = true)]
+public class RefreshTokenModel : DbModel
+{
+    [Required] 
+    public string Token { get; set; } = null!;
+    
+    [Required]
+    public DateTime Issued { get; set; }
+}
