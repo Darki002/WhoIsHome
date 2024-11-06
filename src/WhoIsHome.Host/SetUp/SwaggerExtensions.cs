ce0015a8a74b5bfb7e88328578a2db3e620cc789
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-11-06 10:35:26 +0100

    jwt docs in swagger

--- src/WhoIsHome.Host/SetUp/SwaggerExtensions.cs
+++ src/WhoIsHome.Host/SetUp/SwaggerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using WhoIsHome.Host.Authentication;
 
 namespace WhoIsHome.Host.SetUp;
@@ -9,33 +10,75 @@ public static class SwaggerExtensions
     {
         services.AddSwaggerGen(c =>
         {
-            c.EnableAnnotations();
-            
-            c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
-            {
-                Description = "API Key needed to access the endpoints. API Key should be passed as a request header.",
-                In = ParameterLocation.Header,
-                Name = ApiKeyMiddleware.ApiKeyHeaderName,
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "ApiKeyScheme"
-            });
-            
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "ApiKey"
-                        },
-                        In = ParameterLocation.Header
-                    },
-                    new List<string>()
-                }
-            });
+            c.AddApiKey();
+            c.AddJwt();
         });
         return services;
+    }
+
+    private static void AddJwt(this SwaggerGenOptions c)
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "JwtAuthApp API", Version = "v1" });
+
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = """
+                          JWT Authorization header using the Bearer scheme. 
+                                                Example: 'Bearer 12345abcdef'
+                                                Note: Is NOT used by the /Auth endpoints.
+                          """,
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+        });
+    }
+
+    private static void AddApiKey(this SwaggerGenOptions c)
+    {
+        c.EnableAnnotations();
+            
+        c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+        {
+            Description = "API Key needed to access the endpoints. API Key should be passed as a request header.",
+            In = ParameterLocation.Header,
+            Name = ApiKeyMiddleware.ApiKeyHeaderName,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "ApiKeyScheme"
+        });
+            
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "ApiKey"
+                    },
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
     }
 }
