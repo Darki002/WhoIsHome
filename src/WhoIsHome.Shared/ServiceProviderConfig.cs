c89c88e7c27ca1fcde7465fdff0983a0f33b4b86
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-11-26 19:31:49 +0100

    Create ServiceProviderConfig.cs

---
+++ src/WhoIsHome.Shared/ServiceProviderConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WhoIsHome.Shared.Helper;
+
+namespace WhoIsHome.Shared;
+
+public static class ServiceProviderConfig
+{
+    public static IServiceCollection AddWhoIsHomeShared(this IServiceCollection services)
+    {
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        return services;
+    } 
+}
