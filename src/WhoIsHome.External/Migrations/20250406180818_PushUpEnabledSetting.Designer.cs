13fbbbb2dc1f3db4e27de14cf7a217d82adf53c4
Darki002 <113591988+Darki002@users.noreply.github.com>
2025-04-06 20:08:35 +0200

    PushUpEnabledSetting  Migration

--- src/WhoIsHome.External/Migrations/20250406180818_PushUpEnabledSetting.Designer.cs
+++ src/WhoIsHome.External/Migrations/20250406180818_PushUpEnabledSetting.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhoIsHome.External;
 
@@ -10,9 +11,11 @@ using WhoIsHome.External;
 namespace WhoIsHome.External.Migrations
 {
     [DbContext(typeof(WhoIsHomeContext))]
-    partial class WhoIsHomeContextModelSnapshot : ModelSnapshot
+    [Migration("20250406180818_PushUpEnabledSetting")]
+    partial class PushUpEnabledSetting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
