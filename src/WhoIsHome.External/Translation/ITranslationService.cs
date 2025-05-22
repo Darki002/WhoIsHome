c1e3d25607c8fd757c9fb80581d9994ea1dfb01d
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-05-22 10:18:46 +0200

    support TranslatableString

--- src/WhoIsHome.External/Translation/ITranslationService.cs
+++ src/WhoIsHome.External/Translation/ITranslationService.cs
@@ -6,5 +6,9 @@ public interface ITranslationService
 {
     string this[string key, CultureInfo culture] { get; }
     
-    string Translate(string key, CultureInfo culture, params object[] args);
+    string this[TranslatableString str, CultureInfo culture] { get; }
+    
+    string Translate(string key, CultureInfo culture, params object?[] args);
+
+    string Translate(TranslatableString str, CultureInfo culture);
 }
--- src/WhoIsHome.External/Translation/TranslationService.cs
+++ src/WhoIsHome.External/Translation/TranslationService.cs
@@ -8,11 +8,18 @@ internal class TranslationService : ITranslationService
                                                            ?? throw new ArgumentException("Resource does not exist",
                                                                nameof(key));
 
-    public string Translate(string key, CultureInfo culture, params object[] args)
+    public string this[TranslatableString str, CultureInfo culture] => Translate(str.Value, culture, str.Args);
+
+    public string Translate(string key, CultureInfo culture, params object?[] args)
     {
         var template = this[key, culture];
         return args.Length > 0
             ? string.Format(template, args)
             : template;
+    }
+
+    public string Translate(TranslatableString str, CultureInfo culture)
+    {
+        return Translate(str.Value, culture, str.Args);
     }
 }
