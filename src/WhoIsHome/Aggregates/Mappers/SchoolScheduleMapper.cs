7a2663134d8ea07a0a85ff4e43f91acea4d286b0
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2025-08-22 20:21:53 +0200

    add school schedules

---
+++ src/WhoIsHome/Aggregates/Mappers/SchoolScheduleMapper.cs
@@ -0,0 +1,41 @@
+﻿using WhoIsHome.External.Models;
+
+namespace WhoIsHome.Aggregates.Mappers;
+
+public static class SchoolScheduleMapper
+{
+    public static SchoolSchedule ToAggregate(this SchoolScheduleModel model)
+    {
+        var dinnerTime = new DinnerTime(model.PresenceType!.Value, model.DinnerTime);
+
+        return new SchoolSchedule(
+            model.Id,
+            model.SchoolName,
+            model.DayOfWeek,
+            model.StartTime,
+            model.EndTime,
+            dinnerTime,
+            model.UserId);
+    }
+
+    public static SchoolScheduleModel ToModel(this SchoolSchedule aggregate)
+    {
+        var model = new SchoolScheduleModel
+        {
+            SchoolName = aggregate.SchoolName,
+            DayOfWeek = aggregate.DayOfWeek,
+            StartTime = aggregate.StartTime,
+            EndTime = aggregate.EndTime,
+            PresenceType = aggregate.DinnerTime?.PresenceType,
+            DinnerTime = aggregate.DinnerTime?.Time,
+            UserId = aggregate.UserId
+        };
+
+        if (aggregate.Id.HasValue)
+        {
+            model.Id = aggregate.Id.Value;
+        }
+
+        return model;
+    }
+}
